866ad80040854fa4de1153f5bbfa5838ee83a2cd
Nebih123 <nebihhaziri15@gmail.com>
2024-01-20 17:51:50 +0100

    Pixels

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let container = document.querySelector('#container')
 container.style.cssText = 'width: 150px;display: flex; flex-wrap: wrap'
-let div = '<button>!<button>'
+let div = '<button>!'
 
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 49; i++) {
     container.innerHTML += div
 }
